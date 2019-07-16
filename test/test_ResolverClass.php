9c874204ba406bc4a3779d22e86325168d98d2b6
Fabien Sanchez <fzed51+git@gmail.com>
2019-07-16 10:40:23 +0200

    test | correction de tous les tests

--- test/test_ResolverClass.php
+++ test/test_ResolverClass.php
@@ -2,9 +2,11 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use App\Exception\InternalError\UnresolvableParameter as UnresolvableParameterException;
-use App\ResolverClass;
-use Pimple\Container;
+use InstanceResolver\Exception\UnresolvedException;
+use InstanceResolver\ResolverClass;
+use Psr\Container\ContainerInterface;
+use Pimple\Container as Pimple;
+use Pimple\Psr11\Container;
 
 class A
 {
@@ -13,38 +15,32 @@ class A
 class B
 {
     public function __construct($x)
-    {
-    }
+    { }
 }
 class C
 {
     public function __construct(A $x)
-    {
-    }
+    { }
 }
 class D
 {
     public function __construct(B $x)
-    {
-    }
+    { }
 }
 class E
 {
     public function __construct($x)
-    {
-    }
+    { }
 }
 class F
 {
     public function __construct(E $x)
-    {
-    }
+    { }
 }
 class G
 {
     public function __construct(F $x, C $y)
-    {
-    }
+    { }
 }
 
 function message(string $msg)
@@ -61,56 +57,58 @@ function titre(string $msg)
 }
 
 titre("Initialisation du container");
-$container = new Container();
+$container = new Pimple();
 $container[E::class] = function ($c) {
     return new E("x");
 };
+$container = new Container($container);
+
 
 titre("doit retrouver une instance de classe connu du container");
-$resolver = new ResolverClass($this->container);
+$resolver = new ResolverClass($container);
 $e = $resolver(E::class);
-expect($e)->toBeAnInstanceOf(E::class);
+Assert\boolTest(is_a($e, E::class), "ResolverClass doit fournir un objet d'instance " . E::class);
 
 titre("doit retrouver l'instance container");
-$resolver = new ResolverClass($this->container);
+$resolver = new ResolverClass($container);
 $g = $resolver(Container::class);
-expect($g)->toBeAnInstanceOf(Container::class);
-expect($g)->toBe($this->container);
+Assert\boolTest(is_a($g, Container::class), "ResolverClass doit fournir un objet d'instance " . Container::class);
+Assert\boolTest($g === $container, "ResolverClass doit redonner le container");
 
 titre("doit retrouver l'instance container quand on demande la même interface");
-$resolver = new ResolverClass($this->container);
-$g = $resolver(\Psr\Container\ContainerInterface::class);
-expect($g)->toBeAnInstanceOf(Container::class);
-expect($g)->toBe($this->container);
+$resolver = new ResolverClass($container);
+$g = $resolver(ContainerInterface::class);
+Assert\boolTest(is_a($g, Container::class), "ResolverClass doit fournir un objet d'instance " . Container::class);
+Assert\boolTest($g === $container, "ResolverClass doit redonner le container");
 
 titre("doit instancier une class simple");
-$resolver = new ResolverClass($this->container);
+$resolver = new ResolverClass($container);
 $a = $resolver(A::class);
-expect($a)->toBeAnInstanceOf(A::class);
+Assert\boolTest(is_a($a, A::class), "ResolverClass doit fournir un objet d'instance " . A::class);
 
 titre("ne doit pas instancier une class avec un paramètre non résolvable");
-expect(function () {
-    $resolver = new ResolverClass($this->container);
+Assert\throwTest(function () use ($container) {
+    $resolver = new ResolverClass($container);
     $b = $resolver(B::class);
-})->toThrow(new UnresolvableParameterException());
+}, new UnresolvedException());
 
 titre("doit instancier une class dépendante d'une classe simple");
-$resolver = new ResolverClass($this->container);
+$resolver = new ResolverClass($container);
 $c = $resolver(C::class);
-expect($c)->toBeAnInstanceOf(C::class);
+Assert\boolTest(is_a($c, C::class), "ResolverClass doit fournir un objet d'instance " . C::class);
 
 titre("ne doit pas instancier une class dépendante d'une class avec un paramètre non résolvable");
-expect(function () {
-    $resolver = new ResolverClass($this->container);
+Assert\throwTest(function () use ($container) {
+    $resolver = new ResolverClass($container);
     $d = $resolver(D::class);
-})->toThrow(new UnresolvableParameterException());
+}, new UnresolvedException());
 
 titre("doit instancier une class dépendante d'une classe connu du container");
-$resolver = new ResolverClass($this->container);
+$resolver = new ResolverClass($container);
 $f = $resolver(F::class);
-expect($f)->toBeAnInstanceOf(F::class);
+Assert\boolTest(is_a($f, F::class), "ResolverClass doit fournir un objet d'instance " . F::class);
 
 titre("doit instancier une class complexe");
-$resolver = new ResolverClass($this->container);
+$resolver = new ResolverClass($container);
 $g = $resolver(G::class);
-expect($g)->toBeAnInstanceOf(G::class);
+Assert\boolTest(is_a($g, G::class), "ResolverClass doit fournir un objet d'instance " . G::class);
